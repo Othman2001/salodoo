8fb756efc14e03b84c7c7c0c16da3e60fb4f6951
Othman <67125450+Othman2001@users.noreply.github.com>
2023-03-17 10:44:38 +0200

    init: generate-apps

---
+++ salodoo/apps/backend/src/api/biker/controllers/biker.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * biker controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::biker.biker');
---
+++ salodoo/apps/backend/src/api/biker/routes/biker.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * biker router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::biker.biker');
